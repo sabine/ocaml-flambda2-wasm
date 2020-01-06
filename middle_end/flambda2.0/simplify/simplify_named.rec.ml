62896552d44ad2cbde83b6b11b8d3f63f557bf45
Mark Shinwell <mshinwell@gmail.com>
2020-01-06 17:34:34 +0000

    Lifting

--- middle_end/flambda2.0/simplify/simplify_named.rec.ml
+++ middle_end/flambda2.0/simplify/simplify_named.rec.ml
@@ -202,8 +202,6 @@ type simplify_function_result = {
 let simplify_function dacc closure_id function_decl ~all_function_decls_in_set
       ~closure_bound_names ~closure_element_types ~old_to_new_code_ids =
   let name = Format.asprintf "%a" Closure_id.print closure_id in
-  Format.eprintf "simplify_function %s, closure_bound_names %a\n%!" name
-    (Closure_id.Map.print Name_in_binding_pos.print) closure_bound_names;
   Profile.record_call ~accumulate:true name (fun () ->
     let denv_after_enter_closure = DE.enter_closure (DA.denv dacc) in
     let code_id = FD.code_id function_decl in
@@ -219,7 +217,6 @@ let simplify_function dacc closure_id function_decl ~all_function_decls_in_set
                 closure_id ~all_function_decls_in_set ~closure_bound_names
                 ~closure_element_types ~old_to_new_code_ids)
           in
-Format.eprintf "DA for body of %s:@ %a\n%!" name DA.print dacc;
           match
             Simplify_toplevel.simplify_toplevel dacc body
               ~return_continuation
