ff33ac34bf77db205833d9eed1002015e0d4b957
sabine <sabine@tarides.com>
2020-04-10 16:38:09 +0200

    removing some unnecessary code

--- wasm/wasm_print_wat.ml
+++ wasm/wasm_print_wat.ml
@@ -20,5 +20,3 @@ module Sexpr = Wasm_sexpr
 let instr oc width e = Sexpr.output oc width (Arrange.instr e)
 let func oc width f = Sexpr.output oc width (Arrange.func f)
 let module_ oc width m = Sexpr.output oc width (Arrange.module_ m)
-let script oc width mode s =
-  List.iter (Sexpr.output oc width) (Arrange.script mode s)
