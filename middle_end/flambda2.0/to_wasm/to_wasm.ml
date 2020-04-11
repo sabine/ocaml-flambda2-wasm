c7b483fc6c85d8a6a77930ed2ef79cbb1b938206
sabine <sabine@tarides.com>
2020-04-11 10:53:35 +0200

    every module has a __module_init function that is exported under the name '__module_init'

--- middle_end/flambda2.0/to_wasm/to_wasm.ml
+++ middle_end/flambda2.0/to_wasm/to_wasm.ml
@@ -28,24 +28,30 @@ let todo () = failwith "Not yet implemented"
 let unit (_unit : Flambda_unit.t) = 
   Profile.record_call "flambda2_to_wasm" (fun () ->
     (*let (functions, instructions) = expr env (Flambda_unit.body unit) in*)
-    let start_function_type_name = "_t__module_init" in
-    let start_function_type = FuncType {
-      name = Some start_function_type_name;
+    let module_init_function_type_name = "_t__module_init" in
+    let module_init_function_type = FuncType {
+      name = Some module_init_function_type_name;
       t = ([I32Type],[])
     } in
-    let start_function_type_index = {index = 0l; name = Some start_function_type_name} in
-    let start_function_name = "__module_init" in
-    let start_function_index = {index = 0l; name = Some start_function_name} in
-    let start_function = {
-      name = start_function_name;
-      ftype = start_function_type_index;
+    let module_init_function_type_index = {index = 0l; name = Some module_init_function_type_name} in
+    let module_init_function_name = "__module_init" in
+    let module_init_function_index = {index = 0l; name = Some module_init_function_name} in
+    let module_init_function = {
+      name = module_init_function_name;
+      ftype = module_init_function_type_index;
       locals = [];
-      body = [Call start_function_index];
+      body = [];
+    } in
+    let module_init_function_export = {
+      name = module_init_function_name;
+      edesc = FuncExport module_init_function_index
     } in
 
     { empty_module with
-      types = [start_function_type];
-      funcs = [start_function];
+      imports = [];
+      types = [module_init_function_type];
+      funcs = [module_init_function];
       start = None;
+      exports = [module_init_function_export]
     }
   )
